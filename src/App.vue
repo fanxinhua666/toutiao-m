049802ecfc4a32c2ffac32ce20845d1140b91290
fanxinhua <21113559@qq.com>
2021-04-10 22:47:32 +0800

    动态处理vant为37.5px尺寸和75

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,15 @@
   <div id="app">
     <router-view />
     <h1>黑马头条</h1>
+    <div>
+      <i class="heima hm-gengduo"></i>
+      <van-button type="primary">主要按钮</van-button>
+      <van-button type="info">信息按钮</van-button>
+      <van-button type="default">默认按钮</van-button>
+      <van-button type="warning">警告按钮</van-button>
+      <van-button type="danger">危险按钮</van-button>
+      <div class="box"></div>
+    </div>
   </div>
 </template>
 <script>
@@ -9,4 +18,10 @@ export default {
   name: 'app'
 }
 </script>
-<style lang="less"></style>
+<style lang="less">
+.box {
+  width: 300px;
+  height: 400px;
+  background-color: blue;
+}
+</style>
